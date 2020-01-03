c639ccac04dae939d2b68b8d851e774e548bb094
Jackson Oppenheim <jacksonopp@gmail.com>
2020-01-02 21:35:31 -0600

    added flatlist for restaurants

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,11 +4,18 @@ import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 import useResults from '../hooks/useResults';
+import ResultList from '../components/ResultList';
 
 const SearchScreen = () => {
 
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    return results.filter(result => {
+      return result.price === price
+    })
+  }
 
   return (
     <View>
@@ -18,6 +25,9 @@ const SearchScreen = () => {
         onTermSubmit={() => searchApi(term)} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultList results={filterResultsByPrice("$")} title="Cost Effective" />
+      <ResultList results={filterResultsByPrice("$$")} title="A Little Pricy" />
+      <ResultList results={filterResultsByPrice("$$$")} title="Expensive" />
     </View>
   )
 }
